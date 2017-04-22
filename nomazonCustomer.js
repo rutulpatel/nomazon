288cc1f4301258c5758e564ed84c825339a577db
Rutul Patel <rutul-patel@live.com>
2017-04-21 23:42:35 -0700

    Move code

--- nomazonCustomer.js
+++ nomazonCustomer.js
@@ -2,14 +2,7 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 require('console.table');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'nomazon'
-});
-
+var connection = mysql.createConnection({ host: 'localhost', port: 3306, user: 'root', password: '', database: 'nomazon' });
 connection.connect(function(err) {
     if (err) console.log(err);
 });
